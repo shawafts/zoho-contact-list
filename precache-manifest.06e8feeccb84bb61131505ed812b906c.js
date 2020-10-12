d39b2a99e120892a46c71d4874a4919a40e5c255
Shawaf Mohammed <shawaf.ts@gmail.com>
2020-10-13 03:42:35 +0530

    Updates

---
+++ precache-manifest.06e8feeccb84bb61131505ed812b906c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7cb0c3fd89ef8add5d44dda4f9c768",
+    "url": "/zoho-contact-list/index.html"
+  },
+  {
+    "revision": "59de5a2a7c2d881f46b0",
+    "url": "/zoho-contact-list/static/css/2.590bbf73.chunk.css"
+  },
+  {
+    "revision": "59de5a2a7c2d881f46b0",
+    "url": "/zoho-contact-list/static/js/2.0dddc21f.chunk.js"
+  },
+  {
+    "revision": "4d833975c154ea21126df2a4e5867037",
+    "url": "/zoho-contact-list/static/js/2.0dddc21f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5bc76463c94d9362fc",
+    "url": "/zoho-contact-list/static/js/main.3c102ff8.chunk.js"
+  },
+  {
+    "revision": "49a4550342b58ae00066",
+    "url": "/zoho-contact-list/static/js/runtime-main.b835933f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/zoho-contact-list/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/zoho-contact-list/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/zoho-contact-list/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/zoho-contact-list/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/zoho-contact-list/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
